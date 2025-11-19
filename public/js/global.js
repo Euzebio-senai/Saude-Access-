afe2f964b852d59c68bba8fef2c0012597b33b5b
Euzebio-senai <mateus.e.ferreira@ba.estudante.senai.br>
2025-11-18 23:19:04 -0300

    alteracoes

--- public/js/global.js
+++
@@ -1,8 +0,0 @@
-let html = document.querySelector("html")
-
-document.addEventListener("DOMContentLoaded", () => {
-    let temaSalvo = localStorage.getItem("theme");
-    if (temaSalvo) {
-        html.setAttribute("data-theme", temaSalvo);
-    }
-});
--- src/db/pool.js
+++
@@ -1,9 +0,0 @@
-import pg from 'pg';
-
-const password = ``
-
-const pool = new pg.Pool({
-    connectionString: `postgresql://postgres:${password}@db.dazczejlredrhvwjdzyt.supabase.co:5432/postgres`
-});
-
-export default pool;
